26578d4f01a40876c811e854c1d241b01c4329c2
Ankush291 <ankush.khamaru.2002@gmail.com>
2025-01-06 21:51:10 +0530

    Add login and register page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Product
+from .forms import RegistrationForm
 
 # Create your views here.
 def home(request):
@@ -12,3 +16,43 @@ def home(request):
 def about(request):
     context = {}
     return render(request, 'about.html', context)
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There is something wrong. Try again"))
+            return redirect('login')
+    else:
+        context = {}
+        return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You are logged out"))
+    return redirect('home')
+
+def register_user(request):
+    form = RegistrationForm()
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully for {username}')
+            return redirect('login')
+        else:
+            messages.success(request, ("There is something wrong"))
+            return redirect('register')
+    else:
+        context = {
+            'form': form
+        }
+        return render(request, 'register.html', context)
